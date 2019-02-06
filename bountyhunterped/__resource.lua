196d27b23ff665e78ce05f028989af47f9bd5731
GrimDesigns <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-02-05 20:54:42 -0700

    Add Bounty Hunter Ped
    All peds are working, no updates currently needed

---
+++ bountyhunterped/__resource.lua
@@ -0,0 +1,8 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    's_m_y_sheriff_01.ydd',
+    's_m_y_sheriff_01.yft',
+    's_m_y_sheriff_01.ymt',
+    's_m_y_sheriff_01.ytd',
+}
